3162e4685fb92daa2378672d4bc680b75811e249
Martijn Moreel <martijn.moreel@gmail.com>
2019-03-08 08:59:56 +0100

    Fix unit tests

--- plugin.video.vrt.nu/vrtnutests/vrtplayertests.py
+++ plugin.video.vrt.nu/vrtnutests/vrtplayertests.py
@@ -10,28 +10,28 @@ class TestVRTPlayer(unittest.TestCase):
         mock = MagicMock()
         mock.show_listing()
         player = vrtplayer.VRTPlayer(None,mock,None, vrtapihelper.VRTApiHelper())
-        player.show_videos('/vrtnu/a-z/tussen-nu-en-morgen/2018/tussen-nu-en-morgen.relevant/')
+        player.show_videos('/vrtnu/a-z/tussen-nu-en-morgen/2018/tussen-nu-en-morgen.relevant/', None)
         self.assertTrue(mock.show_listing.called)
 
     def test_show_videos_single_season_shows_videos(self):
         mock = MagicMock()
         mock.show_listing()
         player = vrtplayer.VRTPlayer(None,mock,None, vrtapihelper.VRTApiHelper())
-        player.show_videos('/vrtnu/a-z/apocalyps--de-eerste-wereldoorlog/1/apocalyps--de-eerste-wereldoorlog-s1a3.relevant/')
+        player.show_videos('/vrtnu/a-z/apocalyps--de-eerste-wereldoorlog/1/apocalyps--de-eerste-wereldoorlog-s1a3.relevant/',None)
         self.assertTrue(mock.show_listing.called)
 
     def test_show_videos_multiple_seasons_shows_videos(self):
         mock = MagicMock()
         mock.show_listing()
         player = vrtplayer.VRTPlayer(None,mock,None, vrtapihelper.VRTApiHelper())
-        player.show_videos('%2fvrtnu%2fa-z%2fanimal-babies.relevant%2f')
+        player.show_videos('%2fvrtnu%2fa-z%2fanimal-babies.relevant%2f',None)
         self.assertTrue(mock.show_listing.called)
 
     def test_show_videos_specific_seasons_shows_videos(self):
         mock = MagicMock()
         mock.show_listing()
         player = vrtplayer.VRTPlayer(None,mock,None, vrtapihelper.VRTApiHelper())
-        player.show_videos('/vrtnu/a-z/thuis/24.lists.all-episodes.relevant/')
+        player.show_videos('/vrtnu/a-z/thuis/24.lists.all-episodes.relevant/',None)
         self.assertTrue(mock.show_listing.called)
         
 
