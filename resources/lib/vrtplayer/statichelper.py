439cc6245218b67223450f4f5babe3abdbe70c24
Martijn Moreel <martijn.moreel@gmail.com>
2019-04-13 13:24:13 +0200

    pep-8 fix 2?

--- resources/lib/vrtplayer/statichelper.py
+++ resources/lib/vrtplayer/statichelper.py
@@ -17,7 +17,7 @@ except ImportError:
 
 
 def convert_html_to_kodilabel(text):
-    rep = {"<i>": "[I]", "</i>": "[/I]"} 
+    rep = {"<i>": "[I]", "</i>": "[/I]"}
     rep = dict((re.escape(k), v) for k, v in rep.iteritems())
     pattern = re.compile("|".join(rep.keys()))
     return pattern.sub(lambda m: rep[re.escape(m.group(0))], text)
