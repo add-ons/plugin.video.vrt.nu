3a7634aa69fcdae5e9fa969253f4747884c8200c
Martijn Moreel <martijn.moreel@gmail.com>
2019-04-13 13:45:13 +0200

    Python 2-3 compatibility

--- resources/lib/vrtplayer/statichelper.py
+++ resources/lib/vrtplayer/statichelper.py
@@ -18,7 +18,7 @@ except ImportError:
 
 def convert_html_to_kodilabel(text):
     rep = {"<i>": "[I]", "</i>": "[/I]"}
-    rep = dict((re.escape(k), v) for k, v in rep.iteritems())
+    rep = dict((re.escape(k), v) for k, v in rep.items())
     pattern = re.compile("|".join(rep.keys()))
     return pattern.sub(lambda m: rep[re.escape(m.group(0))], text)
 
