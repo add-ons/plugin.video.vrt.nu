d9742b8a104537240db82421628f554b53b868b9
pietje666 <martijn.moreel@gmail.com>
2019-04-16 11:58:45 +0200

    Html to kodilabelconverter (#129)
    * Html to kodilabelconverter
    
    * Html to kodilabelconverter #128
    
    * Pep-8 fixes
    
    * pep-8 fix 2?
    
    * Python 2-3 compatibility

--- resources/lib/vrtplayer/statichelper.py
+++ resources/lib/vrtplayer/statichelper.py
@@ -3,6 +3,7 @@
 # GNU General Public License v3.0 (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, unicode_literals
+import re
 
 
 # pylint: disable=unused-import
@@ -13,6 +14,13 @@ except ImportError:
 
     def unescape(s):
         return HTMLParser().unescape(s)
+
+
+def convert_html_to_kodilabel(text):
+    rep = {"<i>": "[I]", "</i>": "[/I]"}
+    rep = dict((re.escape(k), v) for k, v in rep.items())
+    pattern = re.compile("|".join(rep.keys()))
+    return pattern.sub(lambda m: rep[re.escape(m.group(0))], text)
 
 
 def strip_newlines(text):
