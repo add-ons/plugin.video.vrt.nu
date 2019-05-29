8ec626a44ca5770d3aa48d3ac5acdd4cf57fe3d7
mediaminister <mediaminister@gmail.com>
2019-05-29 13:08:07 +0200

    fix apihelpertests

--- test/apihelpertests.py
+++ test/apihelpertests.py
@@ -22,7 +22,7 @@ class ApiHelperTests(unittest.TestCase):
 
     def test_get_api_data_single_season(self):
         title_items, sort, ascending, content = self._apihelper.get_episode_items(path='/vrtnu/a-z/het-journaal.relevant/', show_seasons=True)
-        self.assertTrue(121 < len(title_items) < 130, 'We got %s items instead.' % len(title_items))
+        self.assertTrue(121 < len(title_items) < 140, 'We got %s items instead.' % len(title_items))
         self.assertEqual(sort, 'dateadded')
         self.assertFalse(ascending)
         self.assertEqual(content, 'episodes')
