556597fb78392bcb7d635377ffa8e46d6b2bf8f5
Dag Wieers <dag@wieers.com>
2019-09-14 04:24:58 +0200

    More various fixes (#491)
    This PR includes:
    - Various changes to tests

--- test/test_streamservice.py
+++ test/test_streamservice.py
@@ -25,7 +25,7 @@ xbmcgui = __import__('xbmcgui')
 xbmcplugin = __import__('xbmcplugin')
 xbmcvfs = __import__('xbmcvfs')
 
-xbmcaddon.ADDON_SETTINGS['usedrm'] = 'false'
+xbmcaddon.ADDON_SETTINGS['plugin.video.vrt.nu']['usedrm'] = 'false'
 now = datetime.now(dateutil.tz.tzlocal())
 yesterday = now + timedelta(days=-1)
 
@@ -35,7 +35,7 @@ class StreamServiceTests(unittest.TestCase):
     _tokenresolver = TokenResolver(kodi)
     _streamservice = StreamService(kodi, _tokenresolver)
 
-    @unittest.skipIf(not xbmcaddon.ADDON_SETTINGS.get('username') or not xbmcaddon.ADDON_SETTINGS.get('password'), 'Skipping this test by lack of credentials.')
+    @unittest.skipIf(not xbmcaddon.ADDON_SETTINGS.get('plugin.video.vrt.nu').get('username') or not xbmcaddon.ADDON_SETTINGS.get('plugin.video.vrt.nu').get('password'), 'Skipping this test by lack of credentials.')
     def test_get_ondemand_stream_from_invalid_url(self):
         video = dict(video_url='https://www.vrt.be/vrtnu/a-z/het-journaal/2017/het-journaal-het-journaal-laat-20170501/', video_id=None, publication_id=None)
         try:
@@ -44,15 +44,15 @@ class StreamServiceTests(unittest.TestCase):
         except HTTPError:
             pass
 
-    @unittest.skipIf(not xbmcaddon.ADDON_SETTINGS.get('username') or not xbmcaddon.ADDON_SETTINGS.get('password'), 'Skipping this test by lack of credentials.')
+    @unittest.skipIf(not xbmcaddon.ADDON_SETTINGS.get('plugin.video.vrt.nu').get('username') or not xbmcaddon.ADDON_SETTINGS.get('plugin.video.vrt.nu').get('password'), 'Skipping this test by lack of credentials.')
     def test_get_ondemand_stream_from_url_gets_stream_does_not_crash(self):
         video = dict(video_url=yesterday.strftime('https://www.vrt.be/vrtnu/a-z/het-journaal/2019/het-journaal-het-journaal-laat-%Y%m%d/'), video_id=None, publication_id=None)
         stream = self._streamservice.get_stream(video)
         self.assertTrue(stream is not None)
 
     def test_get_mpd_live_stream_from_url_does_not_crash_returns_stream_and_licensekey(self):
-        xbmcaddon.ADDON_SETTINGS['usedrm'] = 'true'
-        xbmcaddon.ADDON_SETTINGS['useinputstreamadaptive'] = 'true'
+        xbmcaddon.ADDON_SETTINGS['plugin.video.vrt.nu']['usedrm'] = 'true'
+        xbmcaddon.ADDON_SETTINGS['plugin.video.vrt.nu']['useinputstreamadaptive'] = 'true'
         video = dict(video_url=CHANNELS[1]['live_stream'], video_id=None, publication_id=None)
         stream = self._streamservice.get_stream(video)
         # NOTE: Testing live streams only works within Europe
@@ -61,8 +61,8 @@ class StreamServiceTests(unittest.TestCase):
 #            self.assertTrue(stream.license_key is not None)
 
     def test_get_hls_live_stream_from_url_does_not_crash_returns_stream_and_licensekey(self):
-        xbmcaddon.ADDON_SETTINGS['usedrm'] = 'false'
-        xbmcaddon.ADDON_SETTINGS['useinputstreamadaptive'] = 'false'
+        xbmcaddon.ADDON_SETTINGS['plugin.video.vrt.nu']['usedrm'] = 'false'
+        xbmcaddon.ADDON_SETTINGS['plugin.video.vrt.nu']['useinputstreamadaptive'] = 'false'
         video = dict(video_url=CHANNELS[1]['live_stream'], video_id=None, publication_id=None)
         stream = self._streamservice.get_stream(video)
         # NOTE: Testing live streams only works within Europe
@@ -70,7 +70,7 @@ class StreamServiceTests(unittest.TestCase):
             self.assertTrue(stream is not None)
 
     def test_get_mpd_live_stream_from_url_does_not_crash(self):
-        xbmcaddon.ADDON_SETTINGS['usedrm'] = 'false'
+        xbmcaddon.ADDON_SETTINGS['plugin.video.vrt.nu']['usedrm'] = 'false'
         video = dict(video_url=CHANNELS[1]['live_stream'], video_id=None, publication_id=None)
         stream = self._streamservice.get_stream(video)
         # NOTE: Testing live streams only works within Europe
@@ -78,8 +78,8 @@ class StreamServiceTests(unittest.TestCase):
             self.assertTrue(stream is not None)
 
     def test_get_hls_live_stream_from_url_does_not_crash(self):
-        xbmcaddon.ADDON_SETTINGS['usedrm'] = 'false'
-        xbmcaddon.ADDON_SETTINGS['useinputstreamadaptive'] = 'false'
+        xbmcaddon.ADDON_SETTINGS['plugin.video.vrt.nu']['usedrm'] = 'false'
+        xbmcaddon.ADDON_SETTINGS['plugin.video.vrt.nu']['useinputstreamadaptive'] = 'false'
         video = dict(video_url=CHANNELS[1]['live_stream'], video_id=None, publication_id=None)
         stream = self._streamservice.get_stream(video)
         # NOTE: Testing live streams only works within Europe
@@ -87,8 +87,8 @@ class StreamServiceTests(unittest.TestCase):
             self.assertTrue(stream is not None)
 
     def test_get_mpd_live_stream_from_id_does_not_crash(self):
-        xbmcaddon.ADDON_SETTINGS['usedrm'] = 'false'
-        xbmcaddon.ADDON_SETTINGS['useinputstreamadaptive'] = 'true'
+        xbmcaddon.ADDON_SETTINGS['plugin.video.vrt.nu']['usedrm'] = 'false'
+        xbmcaddon.ADDON_SETTINGS['plugin.video.vrt.nu']['useinputstreamadaptive'] = 'true'
         video = dict(video_url=None, video_id=CHANNELS[1]['live_stream_id'], publication_id=None)
         stream = self._streamservice.get_stream(video)
         # NOTE: Testing live streams only works within Europe
@@ -102,8 +102,8 @@ class StreamServiceTests(unittest.TestCase):
             self.assertTrue(stream is not None)
 
     def test_get_hls_live_stream_from_id_does_not_crash(self):
-        xbmcaddon.ADDON_SETTINGS['usedrm'] = 'false'
-        xbmcaddon.ADDON_SETTINGS['useinputstreamadaptive'] = 'false'
+        xbmcaddon.ADDON_SETTINGS['plugin.video.vrt.nu']['usedrm'] = 'false'
+        xbmcaddon.ADDON_SETTINGS['plugin.video.vrt.nu']['useinputstreamadaptive'] = 'false'
         video = dict(video_url=None, video_id=CHANNELS[1]['live_stream_id'], publication_id=None)
         stream = self._streamservice.get_stream(video)
         # NOTE: Testing live streams only works within Europe
