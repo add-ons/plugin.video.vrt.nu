2953df445835894b5337c2e1b1d7ada65e782846
Dag Wieers <dag@wieers.com>
2019-11-30 03:09:54 +0100

    Renew ADDON on every invocation (#608)
    * Renew ADDON on every invocation
    
    This is the easiest solution for ensuring we have up-to-date settings.
    But it incurs a small performance hit on every invocation.
    
    * Try something completely different

--- resources/lib/addon_entry.py
+++ resources/lib/addon_entry.py
@@ -3,6 +3,12 @@
 ''' This is the actual VRT NU video plugin entry point '''
 
 from __future__ import absolute_import, division, unicode_literals
-from sys import argv
-from addon import run
-run(argv)
+import kodiutils
+import xbmcaddon
+
+kodiutils.ADDON = xbmcaddon.Addon()
+
+if __name__ == '__main__':
+    from sys import argv
+    from addon import run
+    run(argv)
