e117cf1527ebc0260f710b557e65c287612d4a53
Media Minister <mediaminister@gmail.com>
2019-12-26 12:45:15 +0100

    Fix unit tests

--- test/test_resumepoints.py
+++ test/test_resumepoints.py
@@ -29,15 +29,13 @@ class TestResumePoints(unittest.TestCase):
     _resumepoints = ResumePoints()
     _apihelper = ApiHelper(_favorites, _resumepoints)
 
-    @unittest.skipUnless(addon.settings.get('username'), 'Skipping as VRT username is missing.')
-    @unittest.skipUnless(addon.settings.get('password'), 'Skipping as VRT password is missing.')
-    def test_get_watchlater_episodes(self):
-        """Test items, sort and order"""
-        episode_items, sort, ascending, content = self._apihelper.list_episodes(page=1, variety='watchlater')
-        self.assertTrue(episode_items)
-        self.assertEqual(sort, 'dateadded')
-        self.assertFalse(ascending)
-        self.assertEqual(content, 'episodes')
+    def test_assetpath_to_id(self):
+        """Test converting assetpath to assetid"""
+        self.assertEqual(None, assetpath_to_id(None))
+
+        asset_path = '/content/dam/vrt/2019/08/14/woodstock-depot_WP00157456'
+        asset_id = 'contentdamvrt20190814woodstockdepotwp00157456'
+        self.assertEqual(asset_id, assetpath_to_id(asset_path))
 
     @unittest.skipUnless(addon.settings.get('username'), 'Skipping as VRT username is missing.')
     @unittest.skipUnless(addon.settings.get('password'), 'Skipping as VRT password is missing.')
@@ -53,8 +51,19 @@ class TestResumePoints(unittest.TestCase):
         self.assertFalse(ascending)
         self.assertEqual(content, 'episodes')
 
-        # Remove Winteruur met Lize Feryn (beschikbaar tot 26 maart 2025)
-        self._resumepoints.update(asset_id='contentdamvrt20191015winteruurr005a0001depotwp00162177', title='Winteruur', url='/vrtnu/a-z/winteruur/5/winteruur-s5a1/', watch_later=False, position=635, total=635, whatson_id='705308178527')
+    @unittest.skipUnless(addon.settings.get('username'), 'Skipping as VRT username is missing.')
+    @unittest.skipUnless(addon.settings.get('password'), 'Skipping as VRT password is missing.')
+    def test_get_watchlater_episodes(self):
+        """Test items, sort and order"""
+        episode_items, sort, ascending, content = self._apihelper.list_episodes(page=1, variety='watchlater')
+        self.assertTrue(episode_items)
+        self.assertEqual(sort, 'dateadded')
+        self.assertFalse(ascending)
+        self.assertEqual(content, 'episodes')
+
+    def test_update_none(self):
+        """Test updating empty resumepoints"""
+        self.assertTrue(self._resumepoints.update(asset_id=None, title=None, url=None))
 
     @unittest.skipUnless(addon.settings.get('username'), 'Skipping as VRT username is missing.')
     @unittest.skipUnless(addon.settings.get('password'), 'Skipping as VRT password is missing.')
@@ -70,17 +79,8 @@ class TestResumePoints(unittest.TestCase):
         asset_id, first_entry = next(iter(list(self._resumepoints._data.items())))  # pylint: disable=protected-access
         print('%s = %s' % (asset_id, first_entry))
 
-    def test_assetpath_to_id(self):
-        """Test converting assetpath to assetid"""
-        self.assertEqual(None, assetpath_to_id(None))
-
-        asset_path = '/content/dam/vrt/2019/08/14/woodstock-depot_WP00157456'
-        asset_id = 'contentdamvrt20190814woodstockdepotwp00157456'
-        self.assertEqual(asset_id, assetpath_to_id(asset_path))
-
-    def test_update_none(self):
-        """Test updating empty resumepoints"""
-        self.assertTrue(self._resumepoints.update(asset_id=None, title=None, url=None))
+        # Remove Winteruur met Lize Feryn (beschikbaar tot 26 maart 2025)
+        self._resumepoints.update(asset_id='contentdamvrt20191015winteruurr005a0001depotwp00162177', title='Winteruur', url='/vrtnu/a-z/winteruur/5/winteruur-s5a1/', watch_later=False, position=635, total=635, whatson_id='705308178527')
 
 
 if __name__ == '__main__':
