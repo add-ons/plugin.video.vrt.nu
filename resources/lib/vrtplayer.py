9f905f8e772ff11ec861318fcb5822f229508b26
Siemon Geeroms <siemon.geeroms@globis-software.com>
2021-01-28 21:24:10 +0100

    space issue

--- resources/lib/vrtplayer.py
+++ resources/lib/vrtplayer.py
@@ -8,7 +8,7 @@ from favorites import Favorites
 from helperobjects import TitleItem
 from kodiutils import (colour, delete_cached_thumbnail, end_of_directory, get_addon_info,
                        get_setting, get_setting_bool, get_setting_int, has_credentials,
-                       has_inputstream_adaptive,localize, kodi_version_major, log_error,
+                       has_inputstream_adaptive, localize, kodi_version_major, log_error,
                        ok_dialog, play, set_setting, show_listing,ttl, url_for,
                        wait_for_resumepoints)
 from resumepoints import ResumePoints
