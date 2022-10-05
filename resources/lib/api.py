6dd2aceabd35569de1ad72201f7ebeb40021b270
mediaminister <45148099+mediaminister@users.noreply.github.com>
2022-10-05 22:26:31 +0200

    Fix categories menu (#1052)

--- resources/lib/api.py
+++ resources/lib/api.py
@@ -6,13 +6,26 @@ from __future__ import absolute_import, division, unicode_literals
 
 from helperobjects import TitleItem
 from kodiutils import colour, get_setting_bool, get_setting_int, get_url_json, localize, url_for
-from utils import shorten_link
+from utils import shorten_link, url_to_program
 
 GRAPHQL_URL = 'https://www.vrt.be/vrtnu-api/graphql/v1'
 
 
-def generate_plot(plot, region, product_placement, mpaa, offtime, permalink):
-    """Generate plot"""
+def format_label(program_title, episode_title, program_type, ontime):
+    """Format label"""
+    if program_type == 'mixed_episodes':
+        label = '[B]{}[/B] - {}'.format(program_title, episode_title)
+    elif program_type == 'daily':
+        label = '{} - {}'.format(ontime.strftime('%d/%m'), episode_title)
+    elif program_type == 'oneoff':
+        label = program_title
+    else:
+        label = episode_title
+    return label
+
+
+def format_plot(plot, region, product_placement, mpaa, offtime, permalink):
+    """Format plot"""
     from datetime import datetime
     import dateutil.parser
     import dateutil.tz
@@ -67,7 +80,7 @@ def generate_plot(plot, region, product_placement, mpaa, offtime, permalink):
     return colour(plot)
 
 
-def get_paginated_list(list_id, page_size, end_cursor=''):
+def get_paginated_episodes(list_id, page_size, end_cursor=''):
     """Get paginated list of episodes from GraphQL API"""
     from tokenresolver import TokenResolver
     access_token = TokenResolver().get_token('vrtnu-site_profile_at')
@@ -238,6 +251,156 @@ def get_paginated_list(list_id, page_size, end_cursor=''):
     return data_json
 
 
+def get_paginated_programs(list_id, page_size, end_cursor=''):
+    """Get paginated list of episodes from GraphQL API"""
+    from tokenresolver import TokenResolver
+    access_token = TokenResolver().get_token('vrtnu-site_profile_at')
+    data_json = {}
+    if access_token:
+        headers = {
+            'Authorization': 'Bearer ' + access_token,
+            'Content-Type': 'application/json',
+            'x-vrt-client-name': 'MobileAndroid',
+        }
+        graphql_query = """
+            query PaginatedPrograms(
+              $listId: ID!
+              $endCursor: ID!
+              $pageSize: Int!
+            ) {
+              list(listId: $listId) {
+                __typename
+                ... on PaginatedTileList {
+                  paginated: paginatedItems(first: $pageSize, after: $endCursor) {
+                    edges {
+                      node {
+                        __typename
+                        ...ep
+                      }
+                    }
+                    pageInfo {
+                      startCursor
+                      endCursor
+                      hasNextPage
+                      hasPreviousPage
+                      __typename
+                    }
+                  }
+                }
+              }
+            }
+            fragment ep on ProgramTile {
+              __typename
+              objectId
+              id
+              tileType
+              image {
+                alt
+                templateUrl
+              }
+              title
+              program {
+                title
+                id
+                programType
+                description
+                shortDescription
+                subtitle
+                announcementType
+                announcementValue
+                whatsonId
+                image {
+                  alt
+                  templateUrl
+                }
+                posterImage {
+                  alt
+                  templateUrl
+                }
+              }
+              action {
+                __typename
+                ...action
+              }
+            }
+            fragment action on Action {
+              __typename
+              ... on LinkAction {
+                link
+                linkType
+                __typename
+              }
+            }
+        """
+        payload = dict(
+            operationName='PaginatedPrograms',
+            variables=dict(
+                listId=list_id,
+                endCursor=end_cursor,
+                pageSize=page_size,
+            ),
+            query=graphql_query,
+        )
+        from json import dumps
+        data = dumps(payload).encode('utf-8')
+        data_json = get_url_json(url=GRAPHQL_URL, cache=None, headers=headers, data=data, raise_errors='all')
+    return data_json
+
+
+def convert_programs(api_data, destination):
+    """Convert paginated list of programs to Kodi list items"""
+    programs = []
+    for item in api_data.get('data').get('list').get('paginated').get('edges'):
+        program = item.get('node')
+        program_name = url_to_program(program.get('action').get('link'))
+        path = url_for('programs', program_name=program_name)
+        program_title = program.get('title')
+        label = program_title
+        plot = program.get('description')
+        plotoutline = program.get('subtitle')
+
+        # Art
+        fanart = program.get('program').get('posterImage').get('templateUrl')
+        poster = program.get('program').get('posterImage').get('templateUrl')
+        thumb = program.get('image').get('templateUrl')
+
+        programs.append(
+            TitleItem(
+                label=label,
+                path=path,
+                art_dict=dict(
+                    thumb=thumb,
+                    poster=poster,
+                    banner=fanart,
+                    fanart=fanart,
+                ),
+                info_dict=dict(
+                    title=label,
+                    tvshowtitle=program_title,
+                    plot=plot,
+                    plotoutline=plotoutline,
+                    mediatype='tvshow',
+                ),
+                is_playable=False,
+            )
+        )
+
+    # Paging
+    page_info = api_data.get('data').get('list').get('paginated').get('pageInfo')
+    if page_info.get('hasNextPage'):
+        end_cursor = page_info.get('endCursor')
+        # Add 'More...' entry at the end
+        programs.append(
+            TitleItem(
+                label=colour(localize(30300)),
+                path=url_for(destination, end_cursor=end_cursor),
+                art_dict=dict(thumb='DefaultInProgressShows.png'),
+                info_dict={},
+            )
+        )
+    return programs
+
+
 def convert_episodes(api_data, destination):
     """Convert paginated list of episodes to Kodi list items"""
     import dateutil.parser
@@ -247,22 +410,29 @@ def convert_episodes(api_data, destination):
         video_id = episode.get('watchAction').get('videoId')
         publication_id = episode.get('watchAction').get('publicationId')
         path = url_for('play_id', video_id=video_id, publication_id=publication_id)
-        tvshowtitle = episode.get('program').get('title')
-        label = '[B]{}[/B] - {}'.format(tvshowtitle, episode.get('title'))
-        mpaa = episode.get('ageRaw') or ''
+        program_title = episode.get('program').get('title')
+        program_type = episode.get('program').get('programType')
+
+        # FIXME: Find a better way to determine mixed episodes
+        if destination in ('recent', 'resumepoints_continue'):
+            program_type = 'mixed_episodes'
+        episode_title = episode.get('title')
         offtime = dateutil.parser.parse(episode.get('offTimeRaw'))
+        ontime = dateutil.parser.parse(episode.get('onTimeRaw'))
+        label = format_label(program_title, episode_title, program_type, ontime)
+        mpaa = episode.get('ageRaw') or ''
         product_placement = True if episode.get('productPlacementShortValue') == 'pp' else False
         region = episode.get('regionRaw')
         permalink = episode.get('permalink')
         plot = episode.get('description')
-        plot = generate_plot(plot, region, product_placement, mpaa, offtime, permalink)
+        plot = format_plot(plot, region, product_placement, mpaa, offtime, permalink)
         plotoutline = episode.get('program').get('subtitle')
         duration = int(episode.get('durationSeconds'))
         episode_no = int(episode.get('episodeNumberRaw') or 0)
         season_no = int(''.join(i for i in episode.get('season').get('titleRaw') if i.isdigit()) or 0)
         studio = episode.get('brand').title() if episode.get('brand') else 'VRT'
         aired = dateutil.parser.parse(episode.get('analytics').get('airDate')).strftime('%Y-%m-%d')
-        dateadded = dateutil.parser.parse(episode.get('onTimeRaw')).strftime('%Y-%m-%d %H:%M:%S')
+        dateadded = ontime.strftime('%Y-%m-%d %H:%M:%S')
         year = int(dateutil.parser.parse(episode.get('onTimeRaw')).strftime('%Y'))
         tag = [tag.title() for tag in episode.get('analytics').get('categories').split(',') if tag]
 
@@ -283,7 +453,7 @@ def convert_episodes(api_data, destination):
                 ),
                 info_dict=dict(
                     title=label,
-                    tvshowtitle=tvshowtitle,
+                    tvshowtitle=program_title,
                     aired=aired,
                     dateadded=dateadded,
                     episode=episode_no,
@@ -296,9 +466,6 @@ def convert_episodes(api_data, destination):
                     studio=studio,
                     year=year,
                     tag=tag,
-                    thumb=thumb,
-                    fanart=fanart,
-                    poster=poster,
                     mediatype='episode',
                 ),
                 is_playable=True,
@@ -320,11 +487,41 @@ def convert_episodes(api_data, destination):
     return episodes
 
 
+def get_favorite_programs(end_cursor=''):
+    """Get favorite programs"""
+    page_size = get_setting_int('itemsperpage', default=50)
+    list_id = 'dynamic:/vrtnu.model.json@favorites-list-video'
+    api_data = get_paginated_programs(list_id=list_id, page_size=page_size, end_cursor=end_cursor)
+    programs = convert_programs(api_data, destination='favorites_programs')
+    return programs
+
+
+def get_category_programs(category, end_cursor=''):
+    """Get category programs"""
+    import base64
+    from json import dumps
+    page_size = get_setting_int('itemsperpage', default=50)
+    search_dict = dict(
+        queryString=None,
+        facets=[dict(
+            name='categories',
+            values=[category]
+        )],
+        resultType='watch',
+    )
+    encoded_search = base64.b64encode(dumps(search_dict).encode('utf-8'))
+    list_id = 'uisearch:searchdata@{}'.format(encoded_search.decode('utf-8'))
+
+    api_data = get_paginated_programs(list_id=list_id, page_size=page_size, end_cursor=end_cursor)
+    programs = convert_programs(api_data, destination='categories')
+    return programs
+
+
 def get_continue_episodes(end_cursor=''):
     """Get continue episodes"""
     page_size = get_setting_int('itemsperpage', default=50)
     list_id = 'dynamic:/vrtnu.model.json@resume-list-video'
-    api_data = get_paginated_list(list_id=list_id, page_size=page_size, end_cursor=end_cursor)
+    api_data = get_paginated_episodes(list_id=list_id, page_size=page_size, end_cursor=end_cursor)
     episodes = convert_episodes(api_data, destination='resumepoints_continue')
     return episodes
 
@@ -333,6 +530,58 @@ def get_recent_episodes(end_cursor=''):
     """Get continue episodes"""
     page_size = get_setting_int('itemsperpage', default=50)
     list_id = 'static:/vrtnu/kijk.model.json@par_list_copy_copy_copy'
-    api_data = get_paginated_list(list_id=list_id, page_size=page_size, end_cursor=end_cursor)
-    episodes = convert_episodes(api_data, destination='resumepoints_continue')
+    api_data = get_paginated_episodes(list_id=list_id, page_size=page_size, end_cursor=end_cursor)
+    episodes = convert_episodes(api_data, destination='recent')
     return episodes
+
+
+def get_episodes(program_name, season_name=None, end_cursor=''):
+    """Get episodes"""
+    page_size = get_setting_int('itemsperpage', default=50)
+    if season_name is None:
+        # Check for multiple seasons
+        api_data = get_seasons(program_name)
+        number_of_seasons = len(api_data)
+        if number_of_seasons == 1:
+            season_name = api_data[0].get('name')
+        elif number_of_seasons > 1:
+            seasons = convert_seasons(api_data, program_name)
+            content = 'files'
+            return seasons, content
+
+    if program_name and season_name:
+        list_id = 'static:/vrtnu/a-z/{}/{}.episodes-list.json'.format(program_name, season_name)
+        api_data = get_paginated_episodes(list_id=list_id, page_size=page_size, end_cursor=end_cursor)
+        episodes = convert_episodes(api_data, destination='noop')
+        content = 'episodes'
+        return episodes, content
+    return None
+
+
+def convert_seasons(api_data, program_name):
+    """Convert seasons"""
+    seasons = []
+    for season in api_data:
+        season_title = season.get('title').get('raw')
+        season_name = season.get('name')
+        label = '{} {}'.format(localize(30131), season_title)  # Season X
+        path = url_for('programs', program_name=program_name, season_name=season_name)
+        seasons.append(
+            TitleItem(
+                label=label,
+                path=path,
+                info_dict=dict(
+                    title=label,
+                    mediatype='season',
+                ),
+                is_playable=False,
+            )
+        )
+    return seasons
+
+
+def get_seasons(program_name):
+    """Get seasons"""
+    season_json = get_url_json('https://www.vrt.be/vrtmax/a-z/{}.model.json'.format(program_name))
+    seasons = season_json.get('details').get('data').get('program').get('seasons')
+    return seasons
