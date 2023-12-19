6fdb721fe6737c0abb1f1e6da12469a09234b0cd
mediaminister <mediaminister@gmail.com>
2023-12-19 17:27:16 +0100

    Fix up next test

--- tests/test_api.py
+++ tests/test_api.py
@@ -117,7 +117,7 @@ class TestApi(unittest.TestCase):
 
     def test_upnext(self):
         """Test getting next episode (de-ideale-wereld)"""
-        episode_id = '1666603123838'
+        episode_id = '1700435154519'
         next_episode = get_next_info(episode_id).get('next_episode')
         self.assertTrue(next_episode)
         print(next_episode)
