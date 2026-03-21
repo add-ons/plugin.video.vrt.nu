2fb5fa85bd2f79093569e6667b42239771a3b792
driesenj <120774271+driesenj@users.noreply.github.com>
2026-03-21 01:16:00 +0100

    Decode season_name and update list_id construction
    Decode season_name from base64 format and update list_id accordingly.

--- resources/lib/api.py
+++ resources/lib/api.py
@@ -2316,6 +2316,7 @@ def get_recent_episodes(end_cursor='', use_favorites=False):
 
 def get_episodes(list_id=None, destination=None, end_cursor='', program_name=None, season_name=None, use_favorites=False,
                  feature=None, keywords=None, date=None, channel=None):
+    from base64 import b64decode
     """Get episodes"""
     sort = 'unsorted'
     ascending = True
@@ -2337,12 +2338,17 @@ def get_episodes(list_id=None, destination=None, end_cursor='', program_name=Non
         # reconstruct list_id from program_name and season_name
         if program_name and season_name:
             destination = 'programs'
-            if season_name.startswith('parsys'):
-                list_id = 'static:/vrtnu/a-z/{}.model.json@{}'.format(program_name, season_name)
-            elif season_name.startswith('dynamic_'):
-                list_id = 'dynamic:/vrtnu/a-z/{}.model.json@{}'.format(program_name, season_name.split('dynamic_')[1])
+
+            # season_name is now base64 encoded in a weird format:
+            # o%35|p%/a-z/the-it-crowd/|parsys_container_episodes-list_1|1|o%37|parsys_container_episodes-list_1%|%
+            decoded_season_name = b64decode(season_name[1:]).decode('utf-8').split("%37|")[1].replace("%|%", "")
+            
+            if decoded_season_name.startswith('parsys'):
+                list_id = 'static:/vrtnu/a-z/{}.model.json@{}'.format(program_name, decoded_season_name)
+            elif decoded_season_name.startswith('dynamic_'):
+                list_id = 'dynamic:/vrtnu/a-z/{}.model.json@{}'.format(program_name, decoded_season_name.split('dynamic_')[1])
             else:
-                list_id = 'static:/vrtnu/a-z/{}/{}.episodes-list.json'.format(program_name, season_name)
+                list_id = 'static:/vrtnu/a-z/{}/{}.episodes-list.json'.format(program_name, decoded_season_name)
 
     # kodi paging
     kodi_page_size = get_setting_int('itemsperpage', default=50)
